f3b79dcb7d35088a656fbe98e50fed755efcc7c2
lMiaul <84649279+lMiaul@users.noreply.github.com>
2022-10-16 19:46:28 -0500

    Creacion de los required en la clase Cliente de la base de datos bdproducto

--- TestGitHub/TestGitHub/Models/Cliente.cs
+++ TestGitHub/TestGitHub/Models/Cliente.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TestGitHub.Models
 {
     public partial class Cliente
     {
         public uint IdCliente { get; set; }
+        [Required(ErrorMessage = "El campo NOMBRE es Obligatorio")]
         public string NombreCliente { get; set; } = null!;
+        [Required(ErrorMessage = "El campo APELLIDOS es Obligatorio")]
         public string ApellidosCliente { get; set; } = null!;
+        [Required(ErrorMessage = "El campo EMAIL es obligatorio")]
         public string EmailCliente { get; set; } = null!;
+        [Required(ErrorMessage = "El campo CONTRASEÑA es obligatorio")]
         public string ContraCliente { get; set; } = null!;
     }
 }
