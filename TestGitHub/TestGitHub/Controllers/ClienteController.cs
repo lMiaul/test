c85d5f9a1637db362eb04d348ceb805c1d331ee8
lMiaul <84649279+lMiaul@users.noreply.github.com>
2022-10-20 00:30:19 -0500

    Creacion de controller Cliente, vistas Edit y EditClientes

---
+++ TestGitHub/TestGitHub/Controllers/ClienteController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestGitHub.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly bdproductoContext Context;
+        public ClienteController(bdproductoContext context)
+        {
+            Context = context;
+        }
+        [HttpGet]
+        [Route("cliente/ope/listar")]
+        public IActionResult ListarClientes()
+        {
+            var list = Context.Clientes;
+            return View(list);
+        }
+        [Route("Cliente/Edit/{Codigo}")]
+        public IActionResult Edit(int codigo)
+        {
+
+            var Obj = (from Tcliente in Context.Clientes
+                       where Tcliente.IdCliente == codigo
+                       select Tcliente).Single();
+
+            return View(Obj);
+        }
+        public IActionResult EditarClientes(Cliente ObjNew)
+        {
+            if (ModelState.IsValid)
+            {
+                var ObjOld = (from Tcliente in Context.Clientes
+                              where Tcliente.IdCliente == ObjNew.IdCliente
+                              select Tcliente).Single();
+
+                ObjOld.IdCliente = ObjNew.IdCliente;
+                ObjOld.NombreCliente = ObjNew.NombreCliente;
+                ObjOld.ApellidosCliente = ObjNew.ApellidosCliente;
+                ObjOld.EdadCliente = ObjNew.EdadCliente;
+                ObjOld.TelefonoCliente = ObjNew.TelefonoCliente;
+                ObjOld.DireccionCliente = ObjNew.DireccionCliente;
+                ObjOld.EmailCliente = ObjNew.EmailCliente;
+
+                Context.SaveChanges();
+                return RedirectToAction("ListarClientes");
+            }
+            else
+            {
+                return View("Edit");
+            }
+        }
+
+    }
+}
--- TestGitHub/TestGitHub/Controllers/ProductoController.cs
+++ TestGitHub/TestGitHub/Controllers/ProductoController.cs
@@ -54,13 +54,7 @@ namespace TestGitHub.Controllers
         {
             return View();
         }
-        [HttpGet]
-        [Route("cliente/ope/listar")]
-        public IActionResult ListarClientes()
-        {
-            var list = Context.Clientes;
-            return View(list);
-        }
+        
    
         public IActionResult AddCliente(Cliente obj)
         {
@@ -76,40 +70,5 @@ namespace TestGitHub.Controllers
             }
             
         }
-        [Route("Producto/Edit/{Codigo}")]
-        public IActionResult Edit(int codigo)
-        {
-
-            var Obj = (from Tcliente in Context.Clientes
-                       where Tcliente.IdCliente == codigo
-                       select Tcliente).Single();
-
-            return View(Obj);
-        }
-        public IActionResult EditarClientes(Cliente ObjNew)
-        {
-            if (ModelState.IsValid)
-            {
-                var ObjOld = (from Tcliente in Context.Clientes
-                              where Tcliente.IdCliente == ObjNew.IdCliente
-                              select Tcliente).Single();
-
-                ObjOld.IdCliente = ObjNew.IdCliente;
-                ObjOld.NombreCliente = ObjNew.NombreCliente;
-                ObjOld.ApellidosCliente = ObjNew.ApellidosCliente;
-                ObjOld.EdadCliente = ObjNew.EdadCliente;
-                ObjOld.TelefonoCliente = ObjNew.TelefonoCliente;
-                ObjOld.DireccionCliente = ObjNew.DireccionCliente;
-                ObjOld.EmailCliente = ObjNew.EmailCliente;
-
-                Context.SaveChanges();
-                return RedirectToAction("ListarClientes");
-            }
-            else
-            {
-                return View("EditarClientes");
-            }
-        }
-        
     }
 }
