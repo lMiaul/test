a32a13c71bd36ea252353d2c4ef04f7b20735837
lMiaul <84649279+lMiaul@users.noreply.github.com>
2022-10-24 18:01:58 -0500

    Borrado de los required del modelo Cliente y cambio del class button del registro

--- TestGitHub/TestGitHub/Models/Cliente.cs
+++ TestGitHub/TestGitHub/Models/Cliente.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+
 #nullable disable
 namespace TestGitHub.Models
 {
@@ -8,19 +8,19 @@ namespace TestGitHub.Models
     {
         
         public uint IdCliente { get; set; }
-        [Required(ErrorMessage = "El campo NOMBRE no esta completo.")]
+        
         public string NombreCliente { get; set; } = null!;
-        [Required(ErrorMessage = "El campo APELLIDOS no esta completo.")]
+        
         public string ApellidosCliente { get; set; } = null!;
-        [Required(ErrorMessage = "El campo EDAD no esta completo.")]
+        
         public int EdadCliente { get; set; }
-        [Required(ErrorMessage = "El campo TELEFONO no esta completo.")]
+        
         public int TelefonoCliente { get; set; }
-        [Required(ErrorMessage = "El campo DIRECCION no esta completo.")]
+        
         public string DireccionCliente { get; set; } = null!;
-        [Required(ErrorMessage = "El campo EMAIL no esta completo.")]
+        
         public string EmailCliente { get; set; } = null!;
-        [Required(ErrorMessage = "El campo CONTRASEÑA no esta completo.")]
+        
         public string ContraCliente { get; set; } = null!;
     }
 }
