782780abcdffe4f70c0400b6223560f52aa54206
lMiaul <84649279+lMiaul@users.noreply.github.com>
2022-10-27 00:02:50 -0500

    Cambio de direccion de eso

---
+++ TestGitHub/TestGitHub/Controllers/RepartidorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestGitHub.Controllers
+{
+    public class RepartidorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
