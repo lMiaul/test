994cb6296dac960cde6eec43f6b15a5b25b5e64f
lMiaul <84649279+lMiaul@users.noreply.github.com>
2022-10-28 17:15:37 -0500

    Creacion de Añadir y Listar Repartidores

--- TestGitHub/TestGitHub/Controllers/RepartidorController.cs
+++ TestGitHub/TestGitHub/Controllers/RepartidorController.cs
@@ -13,6 +13,28 @@ namespace TestGitHub.Controllers
         {
             return View();
         }
-        
+        [Route("Repartidor/ope/listar")]
+        public IActionResult ListarRepartidores()
+        {
+            var list = Context.Repartidors;
+            return View(list);
+        }
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        public IActionResult RegistrarRepartidor(Repartidor obj)
+        {
+            if (ModelState.IsValid)
+            {
+                Context.Repartidors.Add(obj);
+                Context.SaveChanges();
+                return RedirectToAction("ListarRepartidores");
+            }
+            else
+            {
+                return RedirectToAction("Registro");
+            }
+        }
     }
 }
